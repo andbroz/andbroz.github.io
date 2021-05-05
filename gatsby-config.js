c44d55e22b5bb28cf3114e0c37d0cd67b5829bd2
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2021-05-06 00:20:17 +0200

    Change web page name and image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Andrzej Web Page`,
+    description: `Andrzej Web Page build with Gatsby.`,
+    author: `@andbroz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -33,4 +33,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,24 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi there</h1>
+    <p>Welcome to Andrzej Web Page</p>
+    <p>
+      Nothing to see here yet. Just started to build my Web Page with Gatsby.
+    </p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/begin.jpg"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Here story begins..."
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
@@ -24,6 +26,6 @@ const IndexPage = () => (
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
