f43579254d77e324cf6d60b13b5f27dbbae5eda1
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-10-30 00:25:34 +0200

    clean up, add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className='text-7xl text-red-700 text-center font-extrabold'>Hello World</h1>
     </div>
   );
 }
