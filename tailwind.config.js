c9819e7bf96884ca5dfa8e29a9306539464e360f
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-11-01 09:00:20 +0100

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-
-// @ts-ignore
+// const defaultConfig = require('tailwindcss/defaultConfig');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      color: {
+        electricBlue: '#373e98',
+        hotPink: '#f16775',
+        shockingYellow: '#fee36e',
+        oldGold: '#ceb92c',
+        darkestGray: '#2a2a2a',
+        complementaryBlue: '#2c41ce',
+      },
       fontFamily: {
         sans: ['Roboto Flex', ...defaultTheme.fontFamily.sans],
         serif: ['Roboto Serif', ...defaultTheme.fontFamily.serif],
