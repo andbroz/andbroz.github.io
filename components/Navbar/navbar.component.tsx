c2e6620e852190c0fa832d1f6f30ff4aedab1b63
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2022-11-01 09:17:39 +0100

    update nav

--- components/Navbar/navbar.component.tsx
+++ components/Navbar/navbar.component.tsx
@@ -1,15 +1,21 @@
 import Link from 'next/link';
 
+const navLinks = [
+  { title: 'Home', href: '/' },
+  { title: 'About', href: '/about' },
+];
+
 export default function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link href='/'>Home</Link>
-        </li>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
+      <ul className='flex flex-row list-none gap-5 text-lg font-sans font-semibold'>
+        {navLinks.map((navLink) => {
+          return (
+            <li key={navLink.title} className='hover:underline'>
+              <Link href={navLink.href}>{navLink.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/navbar.component';
 export default function Header() {
   return (
     <>
-      <div className='flex flex-row justify-end'>
+      <div className='flex flex-row justify-end p-4 container mx-auto'>
         <Navbar />
       </div>
     </>
