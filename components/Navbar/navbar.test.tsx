9a84083a2462baa157073961c1f843ca025b1ff9
Andrzej Brodowicz <andrzej.brodowicz@capgemini.com>
2022-11-02 10:14:03 +0100

    add navbar test

---
+++ components/Navbar/navbar.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { useRouter } from 'next/router';
+import Navbar from './navbar.component';
+
+jest.mock('next/router', () => ({
+  __esModule: true,
+  useRouter: jest.fn(),
+}));
+
+describe('Navbar', () => {
+  it('renders a heading', () => {
+    const mockRouter = {
+      pathname: '/', // the component uses `router.push` only
+    };
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
+
+    render(<Navbar />);
+
+    const home = screen.getByText(/Home/i);
+
+    expect(home).toBeInTheDocument();
+    expect(home.parentElement?.classList).toContain('text-shockingYellow');
+  });
+});
