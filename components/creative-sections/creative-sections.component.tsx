270b92018383a9ccb63ee55a2812da9cba433904
Andrzej Brodowicz <andrzej.brodowicz@gmail.com>
2023-05-06 12:04:09 +0200

    wavy and spikes

--- components/creative-sections/creative-sections.component.tsx
+++ components/creative-sections/creative-sections.component.tsx
@@ -1,4 +1,4 @@
-import style from './creaive-sections.module.css';
+import style from './creative-sections.module.css';
 
 export default function CreativeSections() {
   return (
@@ -6,6 +6,19 @@ export default function CreativeSections() {
       <header>
         <h1 className={style.siteTitle}>Creative Section Shapes</h1>
       </header>
+
+      <section>
+        <div className={style.wrapper}>
+          <h2 className={style.sectionTitle}>Filler</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            eveniet nihil quidem temporibus illum facere tempore dolor possimus,
+            sed iste. Accusantium minima libero hic, deserunt officiis amet odio
+            dolor laborum!
+          </p>
+        </div>
+      </section>
+
 
       <section className={style.diagonal}>
         <div className={style.wrapper}>
@@ -21,7 +34,19 @@ export default function CreativeSections() {
 
       <section>
         <div className={style.wrapper}>
-          <h2 className={style.sectionTitle}></h2>
+          <h2 className={style.sectionTitle}>Some other section</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            eveniet nihil quidem temporibus illum facere tempore dolor possimus,
+            sed iste. Accusantium minima libero hic, deserunt officiis amet odio
+            dolor laborum!
+          </p>
+        </div>
+      </section>
+
+      <section className={style.spikes}>
+        <div className={style.wrapper}>
+          <h2 className={style.sectionTitle}>Spikes</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
             eveniet nihil quidem temporibus illum facere tempore dolor possimus,
@@ -33,7 +58,19 @@ export default function CreativeSections() {
 
       <section>
         <div className={style.wrapper}>
-          <h2 className={style.sectionTitle}></h2>
+          <h2 className={style.sectionTitle}>Filler</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            eveniet nihil quidem temporibus illum facere tempore dolor possimus,
+            sed iste. Accusantium minima libero hic, deserunt officiis amet odio
+            dolor laborum!
+          </p>
+        </div>
+      </section>
+
+      <section className={style.wavy}>
+        <div className={style.wrapper}>
+          <h2 className={style.sectionTitle}>Wavy</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
             eveniet nihil quidem temporibus illum facere tempore dolor possimus,
@@ -45,31 +82,7 @@ export default function CreativeSections() {
 
       <section>
         <div className={style.wrapper}>
-          <h2 className={style.sectionTitle}></h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            eveniet nihil quidem temporibus illum facere tempore dolor possimus,
-            sed iste. Accusantium minima libero hic, deserunt officiis amet odio
-            dolor laborum!
-          </p>
-        </div>
-      </section>
-
-      <section>
-        <div className={style.wrapper}>
-          <h2 className={style.sectionTitle}></h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-            eveniet nihil quidem temporibus illum facere tempore dolor possimus,
-            sed iste. Accusantium minima libero hic, deserunt officiis amet odio
-            dolor laborum!
-          </p>
-        </div>
-      </section>
-
-      <section>
-        <div className={style.wrapper}>
-          <h2 className={style.sectionTitle}></h2>
+          <h2 className={style.sectionTitle}>Filler</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
             eveniet nihil quidem temporibus illum facere tempore dolor possimus,
